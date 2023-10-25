d26547164ff88bacf3dac962c302250bae984331
Nurrohmann <nurrohmanmarduki@gmail.com>
2023-10-25 11:36:43 +0700

    feat: add unit test for hero section

---
+++ components/hero/test/hero.test.tsx
@@ -0,0 +1,35 @@
+import { screen, render } from "@testing-library/react";
+import Hero from "../hero";
+
+describe("check hero text and image", () => {
+  test("check text", () => {
+    const title = "Experience the world, one adventure at a time";
+    const description =
+      "Whether you're looking for a relaxing beach vacation or an adventurous trip to a new city, we have something for everyone.";
+    const location = "Location";
+    const locationText = "Dubai, UAE";
+    const date = "Date";
+    const dateText = "Tue, Nov 10";
+    const price = "Average price";
+    const priceText = "$523 - $849";
+
+    render(<Hero />);
+    const findTitle = screen.getByText(title);
+    const findDesc = screen.getByText(description);
+    const findLocation = screen.getByText(location);
+    const findLocText = screen.getByText(locationText);
+    const findDate = screen.getByText(date);
+    const findDateText = screen.getByText(dateText);
+    const findPrice = screen.getByText(price);
+    const findPriceText = screen.getByText(priceText);
+
+    expect(findTitle).toBeInTheDocument();
+    expect(findDesc).toBeInTheDocument();
+    expect(findLocation).toBeInTheDocument();
+    expect(findLocText).toBeInTheDocument();
+    expect(findDate).toBeInTheDocument();
+    expect(findDateText).toBeInTheDocument();
+    expect(findPrice).toBeInTheDocument();
+    expect(findPriceText).toBeInTheDocument();
+  });
+});
