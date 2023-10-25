f9a88119a6b14c413d71e42d3de0f19b5bcf5f07
Nurrohmann <nurrohmanmarduki@gmail.com>
2023-10-25 09:32:20 +0700

    feat: add style for hero section

---
+++ components/hero/hero.style.ts
@@ -0,0 +1,30 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  width: 100%;
+  padding: 0rem 0 0 0;
+  display: flex;
+  align-items: center;
+`;
+
+export const Information = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+export const HeroTitle = styled.h1`
+  font-family: "Jua", sans-serif;
+  color: #263238;
+  font-size: 3.2rem;
+`;
+
+export const HeroText = styled.p`
+  margin: 0rem 0 1rem 0;
+  color: rgba(38, 50, 56, 0.5);
+  font-size: 1.1rem;
+  font-family: "interRegular", sans-serif;
+`;
+
+export const SearchInfo = styled.div`
+  display: flex;
+`;
--- components/hero/hero.tsx
+++ components/hero/hero.tsx
@@ -1,7 +1,61 @@
+import Image from "next/image";
+import Padding from "../padding/padding";
+import {
+  Container,
+  Information,
+  HeroTitle,
+  HeroText,
+  SearchInfo,
+} from "./hero.style";
+import Romantic from "@/assets/hero/Romantic.png";
+
 export default function Hero() {
   return (
-    <div>
-      <h1>Helo</h1>
-    </div>
+    <Padding>
+      <Container>
+        <Information>
+          <HeroTitle>Experience the world, one adventure at a time</HeroTitle>
+          <HeroText>
+            Whether you're looking for a relaxing beach vacation or an
+            adventurous trip to a new city, we have something for everyone.
+          </HeroText>
+          <SearchInfo>
+            <div>
+              <div>
+                <span>Location</span>
+                <Image src="" alt="" />
+              </div>
+              <div>
+                <p>Dubai, UAE</p>
+              </div>
+            </div>
+            <div>
+              <div>
+                <span>Location</span>
+                <Image src="" alt="" />
+              </div>
+              <div>
+                <p>Dubai, UAE</p>
+              </div>
+            </div>
+            <div>
+              <div>
+                <span>Location</span>
+                <Image src="" alt="" />
+              </div>
+              <div>
+                <p>Dubai, UAE</p>
+              </div>
+            </div>
+            <div>
+              <Image src="" alt="" />
+            </div>
+          </SearchInfo>
+        </Information>
+        <div>
+          <Image src={Romantic} alt="" />
+        </div>
+      </Container>
+    </Padding>
   );
 }
