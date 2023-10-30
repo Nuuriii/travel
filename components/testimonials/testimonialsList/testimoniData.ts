f5619f41c64f2e74f89b87010676202843e96bca
Nurrohmann <nurrohmanmarduki@gmail.com>
2023-10-30 10:24:47 +0700

    feat: add mock data for testimonials

---
+++ components/testimonials/testimonialsList/testimoniData.ts
@@ -0,0 +1,26 @@
+export const testimoniData = [
+  {
+    name: "Liam S.",
+    place: "Hollywood, CA",
+    testimoni:
+      "I booked a custom getaway to Paris (for myself) at great price and the website even helped me book tours and activities. The customer service was excellent and I was reimbursed for lost luggage!",
+  },
+  {
+    name: "Kendra P.",
+    place: "Lake Placid, FL, TX",
+    testimoni:
+      "I booked a custom getaway to Paris (for myself) at great price and the website even helped me book tours and activities. The customer service was excellent and I was reimbursed for lost luggage!",
+  },
+  {
+    name: "Angel C.",
+    place: "Topeka, KS, TX",
+    testimoni:
+      "I booked a custom getaway to Paris (for myself) at great price and the website even helped me book tours and activities. The customer service was excellent and I was reimbursed for lost luggage!",
+  },
+  {
+    name: "Jake B.",
+    place: "Houston, TX",
+    testimoni:
+      "I booked a custom getaway to Paris (for myself) at great price and the website even helped me book tours and activities. The customer service was excellent and I was reimbursed for lost luggage!",
+  },
+];
---
+++ components/testimonials/testimonialsList/testimonialsList.tsx
@@ -0,0 +1,16 @@
+import Image from "next/image";
+
+export default function TestimonialList() {
+  return (
+    <div>
+      <div>
+        <h3></h3>
+        <p></p>
+        <p></p>
+        <div>
+          <img src="" alt="" />
+        </div>
+      </div>
+    </div>
+  );
+}
