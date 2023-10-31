2d11e7c157411864a7b94192f23676eb069af7ad
Nurrohmann <nurrohmanmarduki@gmail.com>
2023-10-31 17:58:37 +0700

    refactor: change element type

--- components/footer/footer.style.ts
+++ components/footer/footer.style.ts
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 
-export const Container = styled.div`
+export const Container = styled.footer`
   width: 100%;
   display: flex;
   background-color: #ddeffd;
